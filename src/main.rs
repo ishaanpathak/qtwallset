d9909af242bb721231f3746e21c6bbd41e8f2d15
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2025-03-29 21:14:13 +0530

    Basic working done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Arg, Command};
 use serde::{Deserialize, Serialize};
 use shellexpand;
+use std::fs::{File, copy, read_dir, remove_file};
+use std::io::Write;
 use std::path::PathBuf;
 use std::process;
 
@@ -88,6 +90,60 @@ fn restart_qtile() {
         .expect("Failed to Restart Qtile!");
 }
 
+fn get_colors_from_image(image_path: &PathBuf) -> Colors {
+    let image_path = image_path.to_str().unwrap();
+    let matugen_output = process::Command::new("sh")
+        .arg("-c")
+        .arg(format!("matugen image {:?} --json hex", image_path))
+        .output()
+        .expect("Failed to execute command");
+
+    let matugen_output_stdout = String::from_utf8_lossy(&matugen_output.stdout).to_string();
+
+    let generated_colours: GeneratedColours =
+        serde_json::from_str(&matugen_output_stdout).expect("Failed to parse JSON");
+
+    generated_colours.colors
+}
+
+fn clear_wallpaper_directory(active_wallpaper_directory: &PathBuf) {
+    if active_wallpaper_directory.is_dir() {
+        for entry in read_dir(active_wallpaper_directory).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_file() {
+                remove_file(path).expect("Failed to clear Wallpaper Directory!");
+            }
+        }
+    }
+}
+
+fn write_wallpaper_config(config_directory: PathBuf, wallpaper_config: WallpaperConfig) {
+    let mut config_file_path = config_directory;
+    config_file_path.push("wallpaper_info.json");
+
+    let mut file = match File::open(&config_file_path) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Config file not found, creating it...");
+            File::create(&config_file_path).expect("Failed to create config file.")
+        }
+    };
+
+    let config_json = serde_json::to_string_pretty(&wallpaper_config).unwrap();
+
+    file.write_all(&config_json.as_bytes())
+        .expect("Failed to write to wallpaper config!");
+}
+
+fn copy_wallpaper(wallpaper_directory: PathBuf, wallpaper_path: PathBuf) {
+    let wallpaper_file_name = wallpaper_path.file_name().unwrap();
+    let mut active_wallpaper_path = wallpaper_directory;
+    active_wallpaper_path.push(wallpaper_file_name);
+    copy(wallpaper_path, active_wallpaper_path)
+        .expect("Failed to copy wallpaper to Qtile Directory");
+}
+
 fn main() {
     static DEFAULT_QTILE_CONFIG_DIRECTORY: &str = "~/.config/qtile";
 
@@ -105,30 +161,34 @@ fn main() {
         .get_matches();
 
     let output_directory = matches.get_one::<String>("output_directory").unwrap();
-    let mut output_directory = PathBuf::from(shellexpand::tilde(output_directory).to_string());
-    output_directory.push("wallpaper");
-    println!("Output Directory: {:?}", output_directory);
+
+    let mut active_wallpaper_directory =
+        PathBuf::from(shellexpand::tilde(output_directory).to_string());
+    active_wallpaper_directory.push("wallpaper");
+    active_wallpaper_directory.push("active");
+
+    let mut wallpaper_info_directory =
+        PathBuf::from(shellexpand::tilde(output_directory).to_string());
+    wallpaper_info_directory.push("cache");
 
     let wallpaper_path = matches.get_one::<String>("wallpaper_path").unwrap();
-    println!("Wallpaper Path: {:?}", wallpaper_path);
+    let wallpaper_path = PathBuf::from(wallpaper_path);
 
-    let generated_colours = process::Command::new("sh")
-        .arg("-c")
-        .arg(format!("matugen image {:?} --json hex", wallpaper_path))
-        .output()
-        .expect("Failed to execute command");
+    let generated_colours = get_colors_from_image(&wallpaper_path);
 
-    let json_string = String::from_utf8_lossy(&generated_colours.stdout).to_string();
-
-    let parsed_data: GeneratedColours =
-        serde_json::from_str(&json_string).expect("Failed to parse JSON");
+    let mut new_wallpaper_path = active_wallpaper_directory.to_path_buf();
+    new_wallpaper_path.push(&wallpaper_path.file_name().unwrap());
 
     let wallpaper_config: WallpaperConfig = WallpaperConfig {
-        file_path: wallpaper_path.to_string(),
-        colors: parsed_data.colors,
+        file_path: new_wallpaper_path.to_string_lossy().to_string(),
+        colors: generated_colours,
     };
 
-    let wallpaper_config_json = serde_json::to_string_pretty(&wallpaper_config).unwrap();
+    clear_wallpaper_directory(&active_wallpaper_directory);
 
-    println!("Wallpaper Config: {}", wallpaper_config_json);
+    copy_wallpaper(active_wallpaper_directory, wallpaper_path);
+
+    write_wallpaper_config(wallpaper_info_directory, wallpaper_config);
+
+    restart_qtile();
 }
